f4eae126ae8f9f9809a93903742354dfafa8ad3a
tchorczewski <62362244+tchorczewski@users.noreply.github.com>
2020-03-19 18:17:28 +0100

    Add files via upload

---
+++ Zadanie 1/Zadanie 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1
+{
+    class Program
+    {
+        public static void Kalkulatorek(int variable1, int variable2)
+        {
+          int suma;
+        int roznica;
+        int iloraz;
+         int iloczyn;
+        suma = variable1 + variable2;
+            roznica = variable1 - variable2;
+            iloraz = variable1/variable2;
+            iloczyn = variable1* variable2;
+
+            Console.WriteLine("Twoje wyniki: ");
+            Console.WriteLine("Suma " + suma);
+            Console.WriteLine("Roznica " + roznica);
+            Console.WriteLine("Iloczyn " + iloczyn);
+            Console.WriteLine("Iloraz " + iloraz);
+
+                       
+           
+        }
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("Podaj dwie liczby:");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Kalkulatorek(a, b);
+
+        }
+    }
+}
