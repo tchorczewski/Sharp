354c6733ac6e9eb63e43b41da44efba5c4238bc0
tchorczewski <62362244+tchorczewski@users.noreply.github.com>
2020-03-24 22:11:23 +0100

    Add files via upload

---
+++ Zadania 11-12/Zadania 11-12/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania_11_12
+{
+    class Program
+    {
+        
+        public static void drawshape(int length, int widith, char sign)
+        {
+
+            Console.SetCursorPosition(1, 1);
+            for(int i = 1; i < length; i++)
+            {
+                for(int j = 1; j < widith; j++)
+                {
+                    Console.SetCursorPosition(i+3,j+2);
+                    
+                    Console.WriteLine(sign);
+                    
+                    
+                    
+                        Console.WriteLine("\n");
+                     
+                }
+                
+            }
+        }
+        public static void factorial()
+        {
+            int liczba;
+            int halp = 1;
+            Console.WriteLine("Podaj liczbę jakiej silnia ma być policzona: ");
+            liczba = int.Parse(Console.ReadLine());
+            for (int i = 1; i < liczba; i++)
+            {
+                halp = halp * (i + 1);
+                Console.WriteLine(halp);
+            }
+            Console.WriteLine(halp);
+        }
+        static void Main(string[] args)
+        {
+            int length;
+            int widith;
+            char sign;
+            Console.WriteLine("Podaj jakie wymiary i z jakiego znaku ma się składać prostokąt");
+            length = Convert.ToInt32(Console.ReadLine());
+            widith = Convert.ToInt32(Console.ReadLine());
+            sign = Convert.ToChar(Console.ReadLine());
+            drawshape(length, widith, sign);
+            factorial();
+        }
+    }
+}
---
+++ Zadania 11-12/Zadania 11-12/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Zadania 11-12")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zadania 11-12")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("44523a04-08de-4b17-be9c-48bf2cb60c56")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
