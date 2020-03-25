75ac80bc15a361a65fe42199143180ed42f2fc58
tchorczewski <62362244+tchorczewski@users.noreply.github.com>
2020-03-25 01:08:12 +0100

    Add files via upload

---
+++ Zadania 13-15/Zadania 13-15/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania_13_15
+{
+    class Program
+    {
+        public static void triangle(int row, char sign)
+        {
+            Console.SetCursorPosition(50, 1);
+            for(int i = 0;i<row;i++)
+            {
+                for(int j = row - i;j>1;j--)
+                {
+                    Console.SetCursorPosition(50 + i, j);
+                    Console.WriteLine(" ");
+                }
+                for(int j = 0; j <= i; j++)
+                {
+                    Console.SetCursorPosition(50 - i, j);
+                    Console.WriteLine("*");
+                }
+                for (int j = row + i; j > 1; j--)
+                {
+                    Console.SetCursorPosition(50 + i, j);
+                    Console.WriteLine(" ");
+                }
+                Console.WriteLine("\n");
+                //Zrobiłem pół więcej nie miałem siły głowić się nad obrazkiem
+            }
+        }
+        public static void loops()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(" " + i);
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine(j);
+                }
+            }
+            for (int i = 4; i > 0; i--)
+            {
+                Console.WriteLine(" " + i);
+                for (int j = 10; j > 0; j--)
+                {
+                    Console.WriteLine(j);
+                }
+            }
+
+
+        }
+
+        public static void emptyrectangle(int length, int widith, char sign)
+        {
+
+            Console.SetCursorPosition(30, 30);
+
+            for (int i = 1; i <= length; i++)
+            {
+                for (int j = 1; j <= widith; j++)
+                {
+                    Console.SetCursorPosition(i + 3, j + 2);
+                    if (i == 1 || i == length || j == 1 || j == widith)
+                    {
+                        Console.SetCursorPosition(i + 3, j + 2);
+                        Console.WriteLine(sign);
+                    }
+                    else
+                    {
+                        Console.WriteLine(" ");
+                    }
+
+                    Console.WriteLine("\n");
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+
+            int lenght;
+            int widith;
+            char sign;
+            int row;
+            Console.WriteLine("Podaj długości boków i znak ramek prostokąta");
+            lenght = int.Parse(Console.ReadLine());
+            widith = int.Parse(Console.ReadLine());
+            sign = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Podaj długość podstawy trójkąta");
+            row = int.Parse(Console.ReadLine());
+            emptyrectangle(lenght, widith, sign);
+            triangle(row, sign);
+            loops();
+            
+        }
+    }
+}
---
+++ Zadania 16-17/Zadania 16-17/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania_16_17
+{
+    class Program
+    {
+        public static void isdividable()
+        {
+            Console.WriteLine("Wypisuję wszystkie liczby do 100,które są podzielne przez 3 i 5");
+            for(int i=0; i < 100; i += 15)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+        public static void candivide(int var2)
+        {
+            Random random1 = new Random();
+            
+            int var1;
+            
+            
+            Console.WriteLine("Generuję dwie liczby z zakresu 100-1000");
+            var1 = random1.Next(100, 1001);
+           
+            Console.WriteLine("Wylosowana liczba 1 to: " + var1);
+            Console.WriteLine("Wylosowana liczba 2 to: " + var2);
+            if (var1 % var2 == 0)
+            {
+                Console.WriteLine("Liczba 1 jest podzielna przez 2");
+            }
+            else
+            {
+                Console.WriteLine("Liczba 1 nie jest podzielna przez 2");
+            }
+            if (var2 % var1 == 0)
+            {
+                Console.WriteLine("Liczba 2 jest podzielna przez 1");
+            }
+            else
+            {
+                Console.WriteLine("Liczba 2 nie jest podzielna przez 1");
+            }
+                 
+
+        }
+        static void Main(string[] args)
+        {
+            Random random2 = new Random();
+            int var2;
+            var2 = random2.Next(100, 1001)+1;//Bo tick zegara
+            isdividable();
+            candivide(var2);
+        }
+    }
+}
