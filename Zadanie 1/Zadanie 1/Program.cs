8b620ff4fb50963b933c5b0f5a8e538616f3242b
tchorczewski <62362244+tchorczewski@users.noreply.github.com>
2020-04-28 15:14:56 +0200

    Add files via upload
    Poprawione zadanie numer 1

--- Zadanie 1/Zadanie 1/Program.cs
+++ Zadanie 1/Zadanie 1/Program.cs
@@ -8,34 +8,134 @@ namespace Zadanie_1
 {
     class Program
     {
-        public static void Kalkulatorek(int variable1, int variable2)
+        public static double Iloraz(double var1, double var2)
         {
-          int suma;
-        int roznica;
-        int iloraz;
-         int iloczyn;
-        suma = variable1 + variable2;
-            roznica = variable1 - variable2;
-            iloraz = variable1/variable2;
-            iloczyn = variable1* variable2;
+            
+            double iloraz;
+            do
+            {
+                if (var1 == 0)
+                {
+                    Console.WriteLine("Podaj wartość numer jeden raz jeszcze");
+                    var1 = isnumber(var1);
+                }
+                if (var2 == 0)
+                {
+                    Console.WriteLine("Podaj wartość numer dwa raz jeszcze");
+                    var2 = isnumber(var2);
 
-            Console.WriteLine("Twoje wyniki: ");
-            Console.WriteLine("Suma " + suma);
-            Console.WriteLine("Roznica " + roznica);
-            Console.WriteLine("Iloczyn " + iloczyn);
-            Console.WriteLine("Iloraz " + iloraz);
+                }
+            }
+            while (var1 == 0 || var2 == 0);
+            iloraz = var1 / var2;
+            return iloraz;
 
-                       
-           
+
+        }
+        public static double Suma(double var1, double var2)
+        {
+            double suma;
+            suma = var1 + var2;
+            return suma;
+
+        }
+        public static double Roznica(double var1, double var2)
+        {
+            double roznica;
+            roznica = var1 - var2;
+            return roznica;
+        }
+        public static double Iloczyn(double var1, double var2)
+        {
+            double iloczyn;
+            iloczyn = var1 * var2;
+            return iloczyn;
+        }
+        public static double isnumber(double var) 
+        {
+            
+            while (true)
+            {
+                try
+                {
+                    var = Convert.ToDouble(Console.ReadLine());
+                    
+                    break;
+                }
+                catch (Exception) 
+                {
+                    Console.WriteLine("Podano błędny znak, spróbuj ponownie");
+                }
+            }
+            return var;
+        }
+        
+        public static void Kalkulatorek()
+        {
+
+            do
+            {
+                double var1 = 0,var2 = 0;
+                int temp = 0;
+                Console.WriteLine("1.Zsumuj wybrane liczby\n" +
+                    "2.Odejmij podane liczby\n" +
+                    "3.Pomnóż przez siebie podane liczby\n" +
+                    "4.Podziel przez siebie podane liczby\n" );
+                temp = Convert.ToInt32(isnumber(temp));
+                
+                    switch (temp)
+                    {
+                        case 1:
+                            Console.WriteLine("Wybrałeś operacje dodawania");
+                            Console.WriteLine("Podaj liczbę numer 1");
+                            var1 = isnumber(var1); 
+                            Console.WriteLine("Podaj liczbę numer 2");
+                            var2 = isnumber(var2);
+                            double suma = Suma(var1, var2);
+                            Console.WriteLine("Wynik dodawania = " + suma);
+                            break;
+                        case 2:
+                            Console.WriteLine("Wybrałeś operację odejmowania");
+                            Console.WriteLine("Podaj liczbę numer 1");
+                            var1 = isnumber(var1);
+                            Console.WriteLine("Podaj liczbę numer 2");
+                            var2 = isnumber(var2);
+                            double roznica = Roznica(var1, var2);
+                            Console.WriteLine("Wynik odejmowania = " + roznica);
+                            break;
+                        case 3:
+                            Console.WriteLine("Wybrałeś operację mnożenia");
+                            Console.WriteLine("Podaj liczbę numer 1");
+                            var1 = isnumber(var1);
+                            Console.WriteLine("Podaj liczbę numer 2");
+                            var2 = isnumber(var2);
+                            double iloczyn = Iloczyn(var1, var2);
+                            Console.WriteLine("Wynik mnożenia = " + iloczyn);
+                            break;
+                        case 4:
+                            Console.WriteLine("Wybrałeś operację dzielenia");
+                            Console.WriteLine("Podaj liczbę numer 1");
+                            var1 = isnumber(var1);
+                            Console.WriteLine("Podaj liczbę numer 2");
+                            var2 = isnumber(var2);
+                            double iloraz = Iloraz(var1, var2);
+                            Console.WriteLine("Wynik dzielenia = " + iloraz);
+                            break;
+
+
+
+                    }
+                    Console.WriteLine("Wciśnij E aby wyjść, dowolny inny klawisz, żeby działać dalej ");
+                
+
+
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.E); 
         }
         static void Main(string[] args)
         {
-            int a;
-            int b;
-            Console.WriteLine("Podaj dwie liczby:");
-            a = Convert.ToInt32(Console.ReadLine());
-            b = Convert.ToInt32(Console.ReadLine());
-            Kalkulatorek(a, b);
+            
+            Kalkulatorek();
 
         }
     }
